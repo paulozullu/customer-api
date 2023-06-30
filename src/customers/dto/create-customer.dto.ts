d94b8f5eb821be8620db5d63be337e13c9ed87a9
Paulo Fabrício <paulozullu@gmail.com>
2023-06-30 10:55:57 -0300

    feat: add CreateCustomerDto

--- src/customers/dto/create-customer.dto.ts
+++ src/customers/dto/create-customer.dto.ts
@@ -1,1 +1,12 @@
-export class CreateCustomerDto {}
+import { IsNotEmpty, IsNumber, IsPositive, IsString } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
+  document: number;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
