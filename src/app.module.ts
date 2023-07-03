b633ee4967b7715ba0188515c70a1b7ff8f06f40
Paulo Fabrício <paulozullu@gmail.com>
2023-07-03 14:16:07 -0300

    feat: added auth resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomersModule } from './customers/customers.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [CustomersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
