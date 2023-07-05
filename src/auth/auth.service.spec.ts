184054918444db13d7009133b5a1e9b19ff2450a
Paulo Fabrício <paulozullu@gmail.com>
2023-07-05 15:30:17 -0300

    feat: added auth test and changed env.example

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { HttpModule } from '@nestjs/axios';
+import { UnauthorizedException } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -16,5 +18,24 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should throw UnauthorizedException on sending invalid username and/or password', async () => {
+    await expect(
+      service.singIn({
+        username: 'f',
+        password: 'f',
+      }),
+    ).rejects.toThrow(UnauthorizedException);
+  });
+
+  it('should return success on entering correct credentials', async () => {
+    const username = 'teste@email.com';
+    const result = service.singIn({
+      username,
+      password: Buffer.from(username).toString('base64'),
+    });
+
+    expect(result).toBeDefined();
   });
 });
